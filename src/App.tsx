3e2ea0d64927af61cb033372e49c7a00e6d02f78
Nicholas Peters <peters.nicholas@gmail.com>
2023-02-20 16:36:16 +0100

    add sailing article

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Header from "./Components/Header"
+import Kayaking from "./Components/Kayaking"
+import Sailing from "./Components/Sailing"
 
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <Kayaking />
+      <Sailing />
     </div>
   )
 }
--- src/Components/Sailing.tsx
+++ src/Components/Sailing.tsx
@@ -1,11 +1,11 @@
-import kajak from '../assets/kajak.jpg';
+import embla from '../assets/embla.jpg'
 
-export default function Kayaking() {
+export default function Sailing() {
     return (
-        <article className="max-w-xl mx-auto mb-52">
-            <img src={kajak} alt="Kayaking" className="" />
+        <article className="max-w-xl mx-auto">
+            <img src={embla} alt="Sailing" className="" />
             <div>
-                <h2 className="my-4 font-bold text-3xl">Sea Kayaking</h2>
+                <h2 className="my-4 font-bold text-3xl">Sailing</h2>
                 <p className="">Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
                     Optio, est quaerat. Voluptates consectetur totam pariatur 
                     architecto facere repellendus iusto quibusdam. Accusamus id 
