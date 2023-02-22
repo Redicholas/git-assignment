feec81ab585b9443242d2ff62dd089f138d576f5
Nicholas Peters <peters.nicholas@gmail.com>
2023-02-22 18:23:09 +0100

    add logo, remove nav

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,14 +1,10 @@
+import logo from '../assets/logo.svg'
+
 export default function Header() {
     return (
-        <header className="p-8 border-b border-black flex items-center justify-between mb-20">
+        <header className="p-8 border-b border-black flex flex-col items-center justify-center mb-20">
+            <img src={logo} height="50" width="50"/>
             <h1>NICHOLAS PETERS</h1>
-                <div>
-                    <ul className="flex gap-4">
-                        <li>Kayaking</li>
-                        <li>Sailing</li>
-                        <li>Cycling</li>
-                    </ul>
-                </div>
         </header>
     )
 }
