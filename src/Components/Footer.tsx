1514542db011a77b98b2a648e1c2492ea0a91a3c
Nicholas Peters <peters.nicholas@gmail.com>
2023-02-23 11:43:13 +0100

    minor change

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="bg-slate-800 text-white p-8 text-center">
+        <footer className="bg-slate-900 text-white p-8 text-center">
             <p>© Nicholas Peters 2023</p>
         </footer>
     )
