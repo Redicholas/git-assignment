cd41ca594bbfdfbd97a97c9af31bce93e68ed9bb
Nicholas Peters <peters.nicholas@gmail.com>
2023-02-23 11:58:02 +0100

    second commit for rebase timeline

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="p-8 border-b border-black flex flex-col items-center justify-center mb-20">
             <img src={logo} height="50" width="50"/>
-            <h1>NICHOLAS C PETERS</h1>
+            <h1>NICHOLAS CHRISTOPHER PETERS</h1>
         </header>
     )
 }
